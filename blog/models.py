22778cf261048bbcc65f5b44e4a9faa5617e3fff
HassanSudan <engsudan143@gmail.com>
2025-03-22 15:19:30 +0300

    Fifty Commit

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    created = models.DateTimeField(auto_now_add=True)
+    created_by = models.IntegerField()
+    
+
