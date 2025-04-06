c5ae0acec0303acd3b56e6bf5be0590b5029b840
HassanSudan <engsudan143@gmail.com>
2025-04-06 14:15:29 +0300

    Intial Commoit 11

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
-from blog.models import Category
+from blog.models import Category, Post
 
 # Create your views here.
 def HomeView(request):
-    return render(request, 'blog/home.html')
+    featurePosts = Post.objects.order_by('-id')[:3]
+    context = {
+        'featurePosts': featurePosts
+    }
+    return render(request, 'blog/home.html', context)
 
 # display Category list
 def CategoryViewList(request):
